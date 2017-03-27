52d2d9c277826c21e86e96dd8ff41764d15909c0
Yaseenh <yaseenh@trackmatic.co.za>
2017-03-27 09:33:41 +0200

    update

--- conf.py
+++ conf.py
@@ -43,10 +43,12 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+#master_doc = 'index'
 .. toctree::
-   :numbered:
+   :caption: Table of Contents
+   :name: index
 
+   foo
 
 # General information about the project.
 project = 'Trackmatic Api'
