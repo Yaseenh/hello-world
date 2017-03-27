05b9542c588af4bdbb35f9e138334a281b675ceb
Yaseenh <yaseenh@trackmatic.co.za>
2017-03-27 09:48:31 +0200

    update

--- conf.py
+++ conf.py
@@ -90,9 +90,7 @@ html_theme = 'sphinx_rtd_theme'#'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
- html_theme_options = {
-        'navigation_depth': 4,
-}
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
